b04c53e3b8449ce3f41c523b7653d821aef2adf2
karangoel16 <karangoel16@yahoo.com>
2017-10-14 16:01:23 -0400

    Speech added

--- code/Speech.py
+++ code/Speech.py
@@ -3,6 +3,7 @@ import pyttsx3 #we will use this to implment module to speak the text when neede
 class speech:
     def __init__(self):
          self.r = sr.Recognizer()
+         self.engine = pyttsx3.init()
     def listen(self):
         with sr.Microphone() as source:
             audio = self.r.listen(source)
@@ -12,7 +13,10 @@ class speech:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
             print("Could not request result from Google Speech Recognition Service; {0}".format(e) )
-
+    def speak(self,text):
+        self.engine.say(text)
+        self.engine.runAndWait()
+        
 if __name__ == "__main__":
     speech=speech()
-    print(speech.listen())
+    speech.speak(speech.listen())
