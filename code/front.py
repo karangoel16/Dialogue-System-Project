6466489ee1303424c10af5a7ca669b910c76d6fc
karangoel16 <karangoel16@yahoo.com>
2017-11-01 10:09:05 -0400

    front end file added

---
+++ code/front.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from bot import Bot
+
+bot = Bot()
+bot.speech.speak("hello how are you? I am wordie your personnel dictionary to know more abour words and learn about it")
+root= Tk()
+frame = Frame(root)
+root.mainloop()
