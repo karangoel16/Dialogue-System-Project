e4f635953944713c1aacf629a50e102e02a0209c
karangoel16 <karangoel16@yahoo.com>
2017-11-03 09:49:55 -0400

    some changes in synonym

--- code/synonym.py
+++ code/synonym.py
@@ -12,3 +12,17 @@ class Synonym:
     def __init__(self,speech):
         self.speech=speech
 
+    def Start_Synonym(self,sent):
+        for i in [key for key,val in nltk.pos_tag(nltk.word_tokenize(sent)) if val== "NN"]:
+            if i != "synonyms" and i != "word":
+                self.Word_Meaning(i)
+    def Word_Synonym(self,word)
+        synList=self.dictionary.synonym(word)
+        print(synList)
+        if(len(synList)>0):
+            self.speech.speak("Other word for" + word + "are")
+            for j in synList:
+                print(j)
+                self.speech.speak(j)
+        else:
+            self.speech.speak("I am sorry . I don't know this word")
